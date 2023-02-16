c49324e7d8885383cc86ea5b12619bd67617ab5b
Sergey Korchagin <millagros77@gmail.com>
2023-02-16 15:27:05 +0300

    code refactored

--- src/Main.java
+++ src/Main.java
@@ -1,80 +1,75 @@
 import java.io.*;
+import java.nio.file.DirectoryNotEmptyException;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Main {
+    protected static StringBuilder log = new StringBuilder();
     public static void main(String[] args) {
-        File src = new File("C:\\Users\\milla\\OneDrive\\Рабочий стол\\Games", "src");
-        File res = new File("C:\\Users\\milla\\OneDrive\\Рабочий стол\\Games", "res");
-        File saveGames = new File("C:\\Users\\milla\\OneDrive\\Рабочий стол\\Games", "saveGames");
-        File temp = new File("C:\\Users\\milla\\OneDrive\\Рабочий стол\\Games", "temp");
-        StringBuilder stringBuilder = new StringBuilder();
-        dirCreate(stringBuilder, src);
-        dirCreate(stringBuilder, res);
-        dirCreate(stringBuilder, saveGames);
-        dirCreate(stringBuilder, temp);
-        File main = new File("C:\\Users\\milla\\OneDrive\\Рабочий стол\\Games\\src", "main");
-        File test = new File("C:\\Users\\milla\\OneDrive\\Рабочий стол\\Games\\src", "test");
-        dirCreate(stringBuilder, main);
-        dirCreate(stringBuilder, test);
-        File Main = new File("C:\\Users\\milla\\OneDrive\\Рабочий стол\\Games\\src\\main", "Main.java");
-        File Utils = new File("C:\\Users\\milla\\OneDrive\\Рабочий стол\\Games\\src\\main", "Utils.java");
-        fileCreate(stringBuilder, Main);
-        fileCreate(stringBuilder, Utils);
-        File drawables = new File("C:\\Users\\milla\\OneDrive\\Рабочий стол\\Games\\res", "drawables");
-        File vectors = new File("C:\\Users\\milla\\OneDrive\\Рабочий стол\\Games\\res", "vectors");
-        File icons = new File("C:\\Users\\milla\\OneDrive\\Рабочий стол\\Games\\res", "icons");
-        dirCreate(stringBuilder, drawables);
-        dirCreate(stringBuilder, vectors);
-        dirCreate(stringBuilder, icons);
-        File tempTxt = new File("C:\\Users\\milla\\OneDrive\\Рабочий стол\\Games\\temp", "temp.txt");
-        fileCreate(stringBuilder, tempTxt);
-        try (FileWriter fileWriter = new FileWriter(tempTxt)) {
-            fileWriter.write(String.valueOf(stringBuilder));
+
+        List<String> directories = new ArrayList<>();
+        directories.addAll(Arrays.asList("C:\\Games\\src","C:\\Games\\res","C:\\Games\\temp","C:\\Games\\saveGames","C:\\Games\\src\\main","C:\\Games\\src\\test",
+                "C:\\Games\\res\\drawables","C:\\Games\\res\\vectors",
+                "C:\\Games\\res\\icons"));
+        for(String element: directories){
+            dirCreate(element);
+        }
+        createNLogFile("C:\\Games\\src\\main", "Main.java");
+        createNLogFile("C:\\Games\\src\\main", "Utils.java");
+        createNLogFile("C:\\Games\\temp", "temp.txt");
+        try (FileWriter fileWriter = new FileWriter("C:\\Games\\temp\\temp.txt", false)) {
+            fileWriter.write(String.valueOf(log));
             fileWriter.flush();
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
 
-        File save1 = new File("C:\\Users\\milla\\OneDrive\\Рабочий стол\\Games\\saveGames", "Save1.dat");
-        File save2 = new File("C:\\Users\\milla\\OneDrive\\Рабочий стол\\Games\\saveGames", "Save2.dat");
-        File save3 = new File("C:\\Users\\milla\\OneDrive\\Рабочий стол\\Games\\saveGames", "Save3.dat");
-        fileCreate(save1);
-        fileCreate(save2);
-        fileCreate(save3);
+        List<String> gameFiles = new ArrayList<>();
+        gameFiles.addAll(Arrays.asList("C:\\Games\\saveGames\\Save1.dat", "C:\\Games\\saveGames\\Save2.dat", "C:\\Games\\saveGames\\Save3.dat"));
+        fileCreate("C:\\Games\\saveGames", "Save1.dat");
+        fileCreate("C:\\Games\\saveGames", "Save2.dat");
+        fileCreate("C:\\Games\\saveGames", "Save3.dat");
+
         GameProgress gameSave1 = new GameProgress(100, 2, 1, 100);
         GameProgress gameSave2 = new GameProgress(90, 3, 5, 500);
         GameProgress gameSave3 = new GameProgress(50, 3, 10, 1000);
-        saveGame("C:\\Users\\milla\\OneDrive\\Рабочий стол\\Games\\saveGames\\Save1.dat", gameSave1);
-        saveGame("C:\\Users\\milla\\OneDrive\\Рабочий стол\\Games\\saveGames\\Save2.dat", gameSave2);
-        saveGame("C:\\Users\\milla\\OneDrive\\Рабочий стол\\Games\\saveGames\\Save3.dat", gameSave3);
-        File saveZip1 = new File("C:\\Users\\milla\\OneDrive\\Рабочий стол\\Games\\saveGames", "SaveZip1.zip");
-        File saveZip2 = new File("C:\\Users\\milla\\OneDrive\\Рабочий стол\\Games\\saveGames", "SaveZip2.zip");
-        File saveZip3 = new File("C:\\Users\\milla\\OneDrive\\Рабочий стол\\Games\\saveGames", "SaveZip3.zip");
-        fileCreate(saveZip1);
-        fileCreate(saveZip2);
-        fileCreate(saveZip3);
-        zipGame("C:\\Users\\milla\\OneDrive\\Рабочий стол\\Games\\saveGames\\Save1.dat", "C:\\Users\\milla\\OneDrive\\Рабочий стол\\Games\\saveGames\\SaveZip1.zip", gameSave1);
-        zipGame("C:\\Users\\milla\\OneDrive\\Рабочий стол\\Games\\saveGames\\Save2.dat", "C:\\Users\\milla\\OneDrive\\Рабочий стол\\Games\\saveGames\\SaveZip2.zip", gameSave2);
-        zipGame("C:\\Users\\milla\\OneDrive\\Рабочий стол\\Games\\saveGames\\Save3.dat", "C:\\Users\\milla\\OneDrive\\Рабочий стол\\Games\\saveGames\\SaveZip3.zip", gameSave3);
-        save1.delete();
-        save2.delete();
-        save3.delete();
+        saveGame("C:\\Games\\saveGames\\Save1.dat", gameSave1);
+        saveGame("C:\\Games\\saveGames\\Save2.dat", gameSave2);
+        saveGame("C:\\Games\\saveGames\\Save3.dat", gameSave3);
+        fileCreate("C:\\Games\\saveGames", "SaveZip.zip");
+        zipGame("C:\\Games\\saveGames\\SaveZip.zip", gameFiles);
+
+        try {
+            Files.delete(Path.of("C:\\Games\\saveGames\\Save1.dat"));
+            Files.delete(Path.of("C:\\Games\\saveGames\\Save2.dat"));
+            Files.delete(Path.of("C:\\Games\\saveGames\\Save3.dat"));
+        } catch (IOException x) {
+            // File permission problems are caught here.
+            System.out.println(x.getMessage());
+        }
     }
-    public static void dirCreate(StringBuilder stringBuilder, File file) {
+     public static void dirCreate(String path) {
+        File file = new File(path);
         if (file.mkdir()) {
-            stringBuilder.append("Папка " + file.getName() + " успешно создана");
-            stringBuilder.append('\n');
+            log.append("Папка " + file.getName() + " успешно создана");
+            log.append('\n');
         } else {
             System.out.println("Что-то пошло не так");
         }
     }
 
-    public static void fileCreate(StringBuilder stringBuilder, File file) {
+    public static void createNLogFile(String path, String name) {
+        File file = new File(path,name);
         try {
             if (file.createNewFile()) {
-                stringBuilder.append("Файл " + file.getName() + " успешно создан");
-                stringBuilder.append('\n');
+                log.append("Файл " + file.getName() + " успешно создан");
+                log.append('\n');
             } else {
                 System.out.println("Что-то пошло не так");
             }
@@ -91,21 +86,24 @@ public class Main {
             System.out.println(e.getMessage());
         }
     }
-    public static void zipGame(String path,String zipPath, GameProgress gameProgress){
-       try (ZipOutputStream zop = new ZipOutputStream(new FileOutputStream(zipPath));
-            FileInputStream fis = new FileInputStream(path)){
+    public static void zipGame(String zipPath, List<String> gameFiles){
+       try (ZipOutputStream zop = new ZipOutputStream(new FileOutputStream(zipPath))){
+            for(String arr : gameFiles){
+            try(FileInputStream fis = new FileInputStream(arr)){
 
-           ZipEntry entry = new ZipEntry(path);
-           zop.putNextEntry(entry);
-           byte[] buffer = new byte[fis.available()];
-           fis.read(buffer);
-           zop.write(buffer);
-           zop.closeEntry();
-       } catch (Exception e){
+               ZipEntry entry = new ZipEntry(arr);
+               zop.putNextEntry(entry);
+               byte[] buffer = new byte[fis.available()];
+               fis.read(buffer);
+               zop.write(buffer);
+               zop.closeEntry();
+           }
+       }} catch (Exception e){
            System.out.println(e.getMessage());
        }
     }
-    public static void fileCreate(File file) {
+    public static void fileCreate(String path, String name) {
+        File file = new File(path,name);
         try {
             file.createNewFile();
         } catch (IOException e) {
